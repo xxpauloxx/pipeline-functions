e24658954e3e6a72c7b25eb799c7b0b851420af1
Paulo Roberto Silva <paulo.pinda@gmail.com>
2022-03-20 19:46:05 -0300

    Fixes setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "pipeline_functions"},
-    packages=setuptools.find_packages(where="pipeline_functions"),
+    packages=setuptools.find_packages(
+        exclude="examples"
+    ),
     python_requires=">=3.6",
 )
